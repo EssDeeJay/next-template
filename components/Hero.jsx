9bfe45b7992ad2d6d957cbb0dcb1a92db684318c
Shreyank Jadiya <yankees@Shreyanks-MacBook-Pro.local>
2023-08-09 23:18:11 -0400

    hero code

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -18,6 +18,35 @@ export default function Hero(){
             <div className="w-[225px] h-[225px] left-[1215px] top-[1042px] absolute bg-amber-300 rounded-full blur-[500px]" />
             </div>
             <Navbar />
+
+            <div className="relative max-w-7xl h-[80vh] flex mx-auto">
+                <div className="w-full lg:w-2/12 text-white flex items-end justify-center order-3 lg:order-1">
+                    <div className="relative">
+                        <img src="https://via.placeholder.com/241x307" alt="placeholder-image" className="rounded-[30px]" />
+                    </div>
+                </div>
+                <div className="w-full lg:w-6/12 text-white flex flex-col items-center justify-center order-1 lg:order-2">
+                    <div className="relative">
+                        <div className="text-white">
+                            <h1 className="text-7xl font-bold text-center"><span className="text-sm font-semibold leading-9">World Class</span> Digital</h1>
+                        </div>
+                        <div className="text-white">
+                            <h1 className="text-7xl font-bold text-left">Agency</h1>
+                        </div>
+                    </div>
+                    <div className="mt-4 px-4">
+                        <div className="text-white w-2/3 mx-auto">
+                            <p>Full support for world's leading B2B brands for creating exceptional layout</p>
+                            <button className="px-6 py-4 bg-amber-300 text-slate-900 rounded-[30px] mt-4">Our Services</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full lg:w-4/12 text-white flex items-center justify-center order-2 lg:order-3">
+                    <div className="relative">
+                        <img src="https://via.placeholder.com/466x475" alt="hero-image" className="rounded-[30px]" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
